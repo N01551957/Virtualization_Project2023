bfd46287e3fc3c93967730e5dc4c04b40ca154c9
Joseph Keaveny <josephkeav95@hotmail.com>
2023-11-03 15:49:39 -0400

    test3

--- main.py
+++ main.py
@@ -29,8 +29,6 @@ def home_page():
             return "There was an issue"
     else:
         return render_template('page.html')
-    #testing123
-    #testing again
 
 if __name__ == '__main__':
-    app.run(debug = True,host='localhost', port=9999) 
+    app.run(debug = True,host='localhost', port=9999)  
