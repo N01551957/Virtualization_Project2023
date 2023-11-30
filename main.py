8a5b8387d385513dae32967f086167c55f680993
joseph keaveny <josephkeav95@hotmail.com>
2023-11-30 14:45:59 -0500

    updated python to resemble github, not 100% done

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'mysql-database-container'
+app.config['MYSQL_HOST'] = 'myapp-mysql'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'Virtualization_project'
